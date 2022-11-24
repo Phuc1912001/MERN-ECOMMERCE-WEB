6ff9dfeff1aa41b50cfe80bbada6ced3fd76b6a6
Phuc1912001 <phucphuc1912001@gmail.com>
2022-11-24 09:05:38 +0700

    setup and conected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
+// db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB ERROR => ", err));
+
 app.get("/users", (req, res) => {
   res.json({
-    data: "phuc phuc",
+    data: "Ryan Zen David Kevin Sara Jane",
   });
 });
 
