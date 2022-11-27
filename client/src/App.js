432157cab0f69864e65695986ba9d52cb02e7e5d
Phuc1912001 <phucphuc1912001@gmail.com>
2022-11-27 08:59:52 +0700

    register , login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Menu from "./components/nav/menu";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Menu />
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Jumbotron from "../components/cards/jumbotron";
+import Jumbotron from "../components/cards/Jumbotron";
 
 const Home = () => {
   return (
