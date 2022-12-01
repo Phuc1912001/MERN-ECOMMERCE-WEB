3b21db1f2b0a0f8dceec9c7d1ab09e555730fdda
Phuc1912001 <phucphuc1912001@gmail.com>
2022-12-01 13:53:01 +0700

    sold , order status

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,13 +1,27 @@
 import express from "express";
 import formidable from "express-formidable";
-import { create, filteredProducts, getToken, list, listProducts, photo, processPayment, productsCount, productsSearch, read, relatedProducts, remove, update } from "../controllers/product.js";
 
 const router = express.Router();
 
 // middlewares
 import { requireSignin, isAdmin } from "../middlewares/auth.js";
 // controllers
-
+import {
+  create,
+  list,
+  read,
+  photo,
+  remove,
+  update,
+  filteredProducts,
+  productsCount,
+  listProducts,
+  productsSearch,
+  relatedProducts,
+  getToken,
+  processPayment,
+  orderStatus,
+} from "../controllers/product.js";
 
 router.post("/product", requireSignin, isAdmin, formidable(), create);
 router.get("/products", list);
@@ -23,4 +37,6 @@ router.get("/related-products/:productId/:categoryId", relatedProducts);
 
 router.get("/braintree/token", getToken);
 router.post("/braintree/payment", requireSignin, processPayment);
+router.put("/order-status/:orderId", requireSignin, isAdmin, orderStatus);
+
 export default router;
